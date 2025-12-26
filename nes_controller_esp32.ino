9d4fb28bf30acf1f45e726b61ea5835a6025856d
paulohlcosta <60619968+paulohlcosta@users.noreply.github.com>
2025-12-25 21:47:07 -0300

    Update nes_controller_esp32.ino
    Modded to use Keyboard instead of Gamepad

--- nes_controller_esp32.ino
+++ nes_controller_esp32.ino
@@ -1,148 +1,120 @@
-/* CONVERTER SINAIS DO GAMEPAD nes CLONE PARA um byte
- * INTERPRETAR ESSE BYTE PELA LIBRARY DO BLEGAMEPAD E ENVIAR OS COMANDOS
- * 
- *                  preto         verde           branco
- *     NC           CLOCK         LATCH           DATA          NC
- *     O            O              O              O              O
- *     
- *                        amarelo                         vermelho
- *           NC           GND             NC              5V        
- *           O             O              O                O
+/* CONVERTER SINAIS DO NES nes CLONE PARA um byte
+ * INTERPRETAR ESSE BYTE
+ * ENVIAR COMNADOS DE KEYBOARD PELA LIBRARY BLEKEYBOARD
  *           
- *  OLHANDO A PCB POR CIMA, ONDE TEM AS SOLDAS DOS FIOS, A SEQUENCIA SEGUE:        
- *   | VCC | GROUND | LATCH | CLOCK | DATA |       
- *           
- *           
- * 1 DPAD DIREITA
+ * 1 DPAD DIREITA    ultimo lido
  * 2 DPAD ESQUERDA
  * 3 DPAD BAIXO 
  * 4 DPAD CIMA
  * 5 START
  * 6 SELECT
  * 7 BOTAO B
- * 8 BOTAO A           
- *           
- *           
- * THANKS TO https://www.youtube.com/watch?v=luMPAOVm8Us
- * https://github.com/lemmingDev/ESP32-BLE-Gamepad
+ * 8 BOTAO A     primeiro lido      
+ *   
+ * CHANGELOG
+ * 1.1 remove bleNES.sendReport() que eu acredito estar em redundancia com press e release.
+ *   -> nao funcionou, ele conecta mas nao recebe os sinais dos botoes
+ * 1.2 em testes (PROBLEMAS NA LEITURA)
+ *      tratamento dos direcionais como eixo X e Y  <
+ *      criado funcao DEBUG com saida serial
+ *      criado funcoes separadas para leitura dos botoes e envio BLE
+ * 1.3 alterado de Gamepad para keyboard - LEITURA OK
  */
 
-#include <Arduino.h>
-#include <BleGamepad.h>
-BleGamepad bleGamepad;
-#define numOfButtons 8
 #define LED_BUILTIN 22
-byte previousButtonStates[numOfButtons];
-byte currentButtonStates[numOfButtons];
+#define nesClock    18      //       NES controller clock pin2
+#define nesLatch    17      //       NES controller latch pin3
+#define nesData     19      //       NES controller data pin4
 
+#include <BleKeyboard.h>
+BleKeyboard bleKeyboard("NES BLE Keyboard", "Paulo ESP32 Lite", 100);
 
+byte nesRegister;            // NES controller button states. 0=pressed 1=released
 const int shiftDelay  = 20;  // delay in microseconds to help with shift register setup/hold timing
-int GAMEPADRegister;            // GAMEPAD controller button states. 0=pressed 1=released
-//int GAMEPADRegister2;    //necessario somente se tiver mais de 8 botoes
-int GAMEPADClock      = 18;      //       GAMEPAD controller clock pin2
-int GAMEPADLatch      = 17;      //       GAMEPAD controller latch pin3
-int GAMEPADData       = 19;      //       GAMEPAD controller data pin4
 
+enum ButtonState { IDLE, PRESSED, RELEASED };
 
-void setup()
-{
+ButtonState buttonUpState = IDLE;
+ButtonState buttonDownState = IDLE;
+ButtonState buttonLeftState = IDLE;
+ButtonState buttonRightState = IDLE;
+ButtonState buttonAState = IDLE;
+ButtonState buttonBState = IDLE;
+ButtonState buttonStartState = IDLE;
+ButtonState buttonSelectState = IDLE;
+
+void setup() {
+  //Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT); digitalWrite(LED_BUILTIN, LOW);
-    for (byte currentPinIndex = 0; currentPinIndex < numOfButtons; currentPinIndex++)
-    {
-        previousButtonStates[currentPinIndex] = HIGH;
-        currentButtonStates[currentPinIndex] = HIGH;
-    }
-    BleGamepadConfiguration bleGamepadConfig;
-    bleGamepadConfig.setAutoReport(false);
-    bleGamepadConfig.setButtonCount(numOfButtons);
-    bleGamepad.begin(&bleGamepadConfig);
-    // changing bleGamepadConfig after the begin function has no effect, unless you call the begin function again
+  pinMode(nesData, INPUT);
+  pinMode(nesClock, OUTPUT);
+  pinMode(nesLatch, OUTPUT);
+  digitalWrite(nesClock, LOW);
+  digitalWrite(nesLatch, LOW);
 
-  // configure pins
-  pinMode(GAMEPADData, INPUT);
-  pinMode(GAMEPADClock, OUTPUT);
-  pinMode(GAMEPADLatch, OUTPUT);
-
-  digitalWrite(GAMEPADClock, LOW);      // GAMEPAD control lines idle low
-  digitalWrite(GAMEPADLatch, LOW);
-
-//  Serial.begin(115200);  Serial.println("Starting work!");
-//  delay(1200);  
+  bleKeyboard.begin();
 
   digitalWrite(LED_BUILTIN, HIGH);
 }
 
+void loop() {
+  if (bleKeyboard.isConnected()) {
+    nesRegister = readNesController();
+    digitalWrite(LED_BUILTIN, HIGH); //LED fica apagado quando conectado
+    handleButton(0, KEY_RIGHT_CTRL, buttonAState, "(A)CTRL"); // ---------------------- nao le
+    handleButton(1, KEY_RIGHT_SHIFT, buttonBState, "(B)SHIFT");
+    handleButton(2, KEY_RETURN, buttonSelectState, "(select)ENTER");
+    handleButton(3, KEY_ESC, buttonStartState, "(start)ESCAPE");
+    handleButton(4, KEY_UP_ARROW, buttonUpState, "UP");
+    handleButton(5, KEY_DOWN_ARROW, buttonDownState, "DOWN");
+    handleButton(6, KEY_LEFT_ARROW, buttonLeftState, "LEFT");
+    handleButton(7, KEY_RIGHT_ARROW, buttonRightState, "RIGHT");
+    delayMicroseconds(shiftDelay * 10); // Small delay to avoid excessive reading
+  } 
+  else {digitalWrite(LED_BUILTIN, LOW);}  //LED fica aceso quando desconectado
+}
 
-void loop()
-{
-  // read the 12 controller buttons into the GAMEPADRegister
-  // GAMEPAD controller button states are asynchronously loaded into the shift register while GAMEPADLatch is high
-  // When GAMEPADLatch goes low, the first data bit is shifted to GAMEPADData
-  // Button data is shifted to GAMEPADData on each low to high transition of GAMEPADClock
- 
-  digitalWrite(GAMEPADLatch, HIGH);       // while the latch pin is high, 
-  delayMicroseconds(shiftDelay);       // impose a delay to allow a setup and hold time
-  digitalWrite(GAMEPADLatch, LOW);  
+byte readNesController() {
+  byte tempData = 255;
 
+  digitalWrite(nesLatch, HIGH); // pulsar LATCH
+  delayMicroseconds(10);
+  if (digitalRead(nesData) == LOW) {bitClear(tempData, 0);}     // o primeiro lido é (A)CTRL
+  digitalWrite(nesLatch, LOW);
+  delayMicroseconds(10);
 
-//-----LEITURA DO BYTE NEGATIVO
-  for (int x = 0; x <= 7; x++) {                    // read in the *8* controller buttons that were latched
-    bitWrite(GAMEPADRegister, x, !digitalRead(GAMEPADData)); // store the current button state on the data input into the correct GAMEPADRegister position
-    digitalWrite(GAMEPADClock, HIGH);                   // generate a clock pulse to bring the next button to the data input
-    delayMicroseconds(shiftDelay);       
-    digitalWrite(GAMEPADClock, LOW);                    
+  for (int i = 1; i < 8; i++) {
+    digitalWrite(nesClock, HIGH); // pulsar CLOCK
+    delayMicroseconds(shiftDelay/10);
+    digitalWrite(nesClock, LOW);
+    delayMicroseconds(shiftDelay);
+
+    if (digitalRead(nesData) == LOW) {bitClear(tempData, i);}     // lê B, select, start, up, down, left, right
+   }
+
+  return tempData;
+}
+  
+void handleButton(int buttonBit, uint8_t key, ButtonState &state, const char* keyName) {
+  switch (state) {
+    case IDLE:
+      if (bitRead(nesRegister, buttonBit) == 0) {
+        bleKeyboard.press(key);  // alterar estado de IDLE para PRESSED
+        state = PRESSED;
+      }
+      break;
+    case PRESSED:
+      if (bitRead(nesRegister, buttonBit) == 1) {
+        bleKeyboard.release(key);
+        state = RELEASED;
+      }
+      break;
+    case RELEASED:
+      if (bitRead(nesRegister, buttonBit) == 0) {
+        state = PRESSED;
+      } else {
+        state = IDLE;  // alterar estado de RELEASED para IDLE
+      }
+      break;
   }
-
-/*-----LEITURA DO BYTE POSITIVO --------------- nao eh necessario quando tem ate 8 botoes
-  for (int x = 0; x <= 7; x++) {                    // read in the *16* controller buttons that were latched
-    bitWrite(GAMEPADRegister2, x, digitalRead(GAMEPADData)); // store the current button state on the data input into the correct GAMEPADRegister position
-    digitalWrite(GAMEPADClock, HIGH);                   // generate a clock pulse to bring the next button to the data input
-    delayMicroseconds(shiftDelay);       
-    digitalWrite(GAMEPADClock, LOW);                    
-  }*/
-
- //termina a etapa de leitura dos botoes fisicos
- //aqui eu tenho uma variavel GAMEPADRegister com os status dos botoes
- 
-/*  Serial.println(":-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:");
-  Serial.println("leitura:");
-  Serial.println(byte(GAMEPADRegister), BIN);
-  Serial.println(":");  //-----------------------------------------------*/
-
-    if (bleGamepad.isConnected())
-    {
-        digitalWrite(LED_BUILTIN, HIGH); //LED fica apagado quando conectado
-        
-        byte GAMEPADRegisterByte = byte(GAMEPADRegister);  //a partir daqui vou converter INT para BYTE
-        for (byte currentIndex = 0; currentIndex < numOfButtons; currentIndex++)
-        {
-        currentButtonStates[currentIndex] = bitRead(GAMEPADRegisterByte, 7-currentIndex);
-//        Serial.print(currentButtonStates[currentIndex], BIN);
-
-            if (currentButtonStates[currentIndex] != previousButtonStates[currentIndex])
-            {
-                if (currentButtonStates[currentIndex] == HIGH)
-                {
-                    bleGamepad.press(currentIndex+1);
-                }
-                else
-                {
-                    bleGamepad.release(currentIndex+1);
-                }
-            }
-        }
-
-        if (currentButtonStates != previousButtonStates)
-        {
-            for (byte currentIndex = 0; currentIndex < numOfButtons; currentIndex++)
-            {
-                previousButtonStates[currentIndex] = currentButtonStates[currentIndex];
-            }
-
-            bleGamepad.sendReport();
-        }
-
-        delay(20);
-    }
-    else {digitalWrite(LED_BUILTIN, LOW);}
 }
